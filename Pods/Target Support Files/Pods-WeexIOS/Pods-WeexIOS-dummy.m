7e32aa8d416e0af9866a7f87c0391bd8d6b19849
yfan <yfemail@163.com>
2016-12-06 15:02:33 +0800

    集成weex sdk

---
+++ Pods/Target Support Files/Pods-WeexIOS/Pods-WeexIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeexIOS : NSObject
+@end
+@implementation PodsDummy_Pods_WeexIOS
+@end
---
+++ WeexIOS/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  WeexIOS
+//
+//  Created by yangfan on 16/12/6.
+//  Copyright © 2016年 yfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
